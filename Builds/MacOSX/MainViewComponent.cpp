8a4f1b476050e8c7c38f6177e35386ed42885547
Rodrigo Sena <senarodrigo@gmail.com>
2015-10-22 22:01:14 -0700

    scrolling fixed

---
+++ Builds/MacOSX/MainViewComponent.cpp
@@ -0,0 +1,9 @@
+//
+//  MainViewComponent.cpp
+//  ChuckPluginTest4
+//
+//  Created by Rodrigo Sena on 10/22/15.
+//
+//
+
+#include "MainViewComponent.h"
---
+++ Builds/MacOSX/MainViewComponent.h
@@ -0,0 +1,14 @@
+//
+//  MainViewComponent.h
+//  ChuckPluginTest4
+//
+//  Created by Rodrigo Sena on 10/22/15.
+//
+//
+
+#ifndef __ChuckPluginTest4__MainViewComponent__
+#define __ChuckPluginTest4__MainViewComponent__
+
+#include <stdio.h>
+
+#endif /* defined(__ChuckPluginTest4__MainViewComponent__) */
