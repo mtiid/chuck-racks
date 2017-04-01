5f252450e37e761cd8055041bd80c4c6661e1303
Jordan Hochenbaum <jordan@kadenze.com>
2017-04-01 14:29:26 -0700

    Making sure to track tab listener class

---
+++ Source/CRTabbedComponentListener.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    CRTabbedComponentListener.h
+    Created: 31 Mar 2017 12:49:32am
+    Author:  Jordan Hochenbaum
+
+  ==============================================================================
+*/
+
+#ifndef CRTABBEDCOMPONENTLISTENER_H_INCLUDED
+#define CRTABBEDCOMPONENTLISTENER_H_INCLUDED
+
+class CRTabbedComponentListener{
+    
+public:
+
+    CRTabbedComponentListener(){};
+    virtual ~CRTabbedComponentListener(){};
+    
+    virtual void removeTabComponent(int tabIndex){};
+    
+private:
+    
+};
+
+
+
+#endif  // CRTABBEDCOMPONENTLISTENER_H_INCLUDED
